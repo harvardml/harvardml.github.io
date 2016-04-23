ca45ec6abd898ddb1bdc3d64887c01d293c60ea7
Sasha Rush <srush@seas.harvard.edu>
2016-04-23 12:05:44 -0400

    .

--- _site/auto/reading.el
+++
@@ -1,6 +0,0 @@
-(TeX-add-style-hook
- "reading"
- (lambda ()
-   (LaTeX-add-bibitems
-    "collobert2011natural")))
-
--- _site/push.sh
+++
@@ -1,2 +0,0 @@
-jekyll build
-scp -r _site/* srush@pub-aws.seas.harvard.edu:/seas/web/sites_static/nlp.seas.harvard.edu/htdocs/
