66e17b0f0a28b8dd50311d32aa4f5d8f59b0fe65
Sasha Rush <srush@seas.harvard.edu>
2016-04-23 11:25:53 -0400

    .

---
+++ _site/auto/reading.el
@@ -0,0 +1,6 @@
+(TeX-add-style-hook
+ "reading"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "collobert2011natural")))
+
---
+++ meetings/auto/reading.el
@@ -0,0 +1,16 @@
+(TeX-add-style-hook
+ "reading"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "collobert2011natural"
+    "2016arXiv160100372L"
+    "DBLP:journals/corr/KimJSR15"
+    "DBLP:conf/aaai/MitchellCHTBCMG15"
+    "DBLP:conf/nips/Graves11"
+    "2015arXiv151102301H"
+    "2015arXiv150900838M"
+    "alphago"
+    "DBLP:journals/corr/DaiL15a"
+    "readcomp"
+    "compresscnn")))
+
